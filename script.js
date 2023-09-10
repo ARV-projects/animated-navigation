d4419011a1ddc19abf936ba950d1d3a41991baf6
ARV-projects <alexander.raducan@gmail.com>
2023-09-10 13:20:28 +0300

    nav bar

--- script.js
+++ script.js
@@ -12,13 +12,35 @@ function toggleNav() {
     // Toggle: Menu Active
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
-        // Animate In - Overlay
+        // Animate IN - Overlay
         overlay.classList.remove('overlay-slide-left');
         overlay.classList.add('overlay-slide-right');
+        // Animate IN - Nav Items
+        nav1.classList.remove('slide-out-1');
+        nav1.classList.add('slide-in-1');
+        nav2.classList.remove('slide-out-2');
+        nav2.classList.add('slide-in-2');
+        nav3.classList.remove('slide-out-3');
+        nav3.classList.add('slide-in-3');
+        nav4.classList.remove('slide-out-4');
+        nav4.classList.add('slide-in-4');
+        nav5.classList.remove('slide-out-5');
+        nav5.classList.add('slide-in-5');
     } else {
-        // Animate Out - Overlay
+        // Animate OUT - Overlay
         overlay.classList.remove('overlay-slide-right');
         overlay.classList.add('overlay-slide-left');
+        // Animate OUT - Nav Items
+        nav1.classList.remove('slide-in-1');
+        nav1.classList.add('slide-out-1');
+        nav2.classList.remove('slide-in-2');
+        nav2.classList.add('slide-out-2');
+        nav3.classList.remove('slide-in-3');
+        nav3.classList.add('slide-out-3');
+        nav4.classList.remove('slide-in-4');
+        nav4.classList.add('slide-out-4');
+        nav5.classList.remove('slide-in-5');
+        nav5.classList.add('slide-out-5');
     }
 }
 
